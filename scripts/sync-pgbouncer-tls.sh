67ec156175beccdd508ee6726ec1bce29787ee19
Rahul Sharma <rs3296472t@gmail.com>
2026-07-11 18:04:53 +0530

    Allow Docker to traverse PgBouncer TLS directory

--- scripts/sync-pgbouncer-tls.sh
+++ scripts/sync-pgbouncer-tls.sh
@@ -6,7 +6,9 @@ CERT_DIR=${CERT_DIR:-/etc/letsencrypt/live/db.redbotleads.com}
 TARGET_DIR="$ROOT_DIR/deploy/tls"
 PGBOUNCER_GID=${PGBOUNCER_GID:-70}
 
-install -d -m 750 -o root -g "$PGBOUNCER_GID" "$TARGET_DIR"
+# Docker must be able to traverse the directory while creating build contexts.
+# The private key remains protected separately with mode 640.
+install -d -m 755 -o root -g "$PGBOUNCER_GID" "$TARGET_DIR"
 install -m 644 -o root -g "$PGBOUNCER_GID" "$CERT_DIR/fullchain.pem" "$TARGET_DIR/fullchain.pem"
 install -m 640 -o root -g "$PGBOUNCER_GID" "$CERT_DIR/privkey.pem" "$TARGET_DIR/privkey.pem"
 
